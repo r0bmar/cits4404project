5c80872ee25124bf0d58c1be179e901a93f3e3a4
MadeleineLim <madeleinelim@Mac-mini.localdomain>
2019-10-14 19:17:51 +0800

    adjust stock price using split coefficient

--- src/gym_pairs_trading/envs/data_source.py
+++ src/gym_pairs_trading/envs/data_source.py
@@ -20,16 +20,22 @@ class DataSource(object):
             self.d1 = self.get_from_cache_or_download_and_cache(symbol_1, today, size)
             self.d2 = self.get_from_cache_or_download_and_cache(symbol_2, today, size)
         else:
-            self.d1, _ = TIME_SERIES_GETTER.get_daily(symbol_1, outputsize=size)
-            self.d2, _ = TIME_SERIES_GETTER.get_daily(symbol_2, outputsize=size)
+            self.d1, _ = TIME_SERIES_GETTER.get_daily_adjusted(symbol_1, outputsize=size)
+            self.d2, _ = TIME_SERIES_GETTER.get_daily_adjusted(symbol_2, outputsize=size)
 
         self.starting_date = max(min(self.d1.index), min(self.d2.index)) # highest start date
         self.end_date      = min(max(self.d1.index), max(self.d2.index)) # lowest end date
 
         self.current_day = self.starting_date
 
+        self.s1_split_coefficient = 1
+        self.s2_split_coefficient = 1
+
     def __iter__(self):
         self.current_day = self.starting_date
+
+        self.s1_split_coefficient = 1
+        self.s2_split_coefficient = 1
         return self
 
     def __next__(self):
@@ -42,11 +48,14 @@ class DataSource(object):
             except KeyError:
                 continue
 
-            s1_open  = s1['1. open']
-            s1_close = s1['4. close']
+            self.s1_split_coefficient *= s1['8. split coefficient']
+            self.s2_split_coefficient *= s2['8. split coefficient']
 
-            s2_open  = s2['1. open']
-            s2_close = s2['4. close']
+            s1_open  = s1['1. open']  * self.s1_split_coefficient
+            s1_close = s1['4. close'] * self.s1_split_coefficient
+
+            s2_open  = s2['1. open']  * self.s2_split_coefficient
+            s2_close = s2['4. close'] * self.s2_split_coefficient
 
             # Calculate percentage change in stock price
             s1_percent_change = (s1_close - s1_open) / s1_open
@@ -57,6 +66,8 @@ class DataSource(object):
 
     def reset(self):
         self.current_day = self.starting_date
+        self.s1_split_coefficient = 1
+        self.s2_split_coefficient = 1
 
     def get_from_cache_or_download_and_cache(self, symbol, date, size):
         file_name = f"{CACHE_DIRECTORY}/{symbol}_{size}_{date}.csv"
@@ -67,7 +78,7 @@ class DataSource(object):
 
             return data
         else:
-            data, _ = TIME_SERIES_GETTER.get_daily(symbol, outputsize=size)
+            data, _ = TIME_SERIES_GETTER.get_daily_adjusted(symbol, outputsize=size)
             data.to_csv(file_name, index_label=False)
             return data
 
