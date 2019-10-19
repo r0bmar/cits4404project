ea1df6620a3b7ec1662e29fdae798a0b1f8b97a7
Asaf Silman <asafsilman@gmail.com>
2019-10-19 15:14:43 +0800

    Added PairsTradingEnvV2 to repo

--- src/gym_pairs_trading/envs/pairs_trading_env.py
+++ src/gym_pairs_trading/envs/pairs_trading_env.py
@@ -4,7 +4,7 @@ from gym.utils import seeding
 import numpy as np
 
 from .data_source import DataSource
-from .trading_sim import TradingSim, Actions
+from .trading_sim import TradingSim, TradingSimV2, Actions
 from .market_metrics import MarketMetrics
 
 import matplotlib.pyplot as plt
@@ -155,16 +155,170 @@ class PairsTradingEnv(gym.Env):
         else:
             print("Invalid render mode")
 
+class PairsTradingEnvV2(gym.Env):
+    metadata = {'render.modes': ['human', 'console']}
+
+    action_space = spaces.Box(low=0, high=1, shape=(3,), dtype=np.float32)
+    observation_space = spaces.Box(
+        low=-1,
+        high=1,
+        shape=(3,)
+    )
+
+    def __init__(self, data_1, data_2, **kwargs):
+        super(PairsTradingEnvV2, self).__init__()
+        self.data_source = DataSource(data_1, data_2, **kwargs)
+        self.trading_sim = TradingSimV2(**kwargs)
+        self.market_metrics = MarketMetrics()
+
+        self.trading_day = 0
+        self.previous_balance = self.trading_sim.get_NAV()
+
+        self.render_data = {'buy': [], 'sell': [], 'hold': [], 'portfolio_value': [], 'spread': []}
+        self.plot_data = {}
+
+    def seed(self, seed=None):
+        self.np_random, seed = seeding.np_random(seed)
+        return [seed]
+
+    def reset(self):
+        self.data_source.reset()
+        self.trading_sim.reset()
+        self.market_metrics.reset()
+
+        self.trading_day = 1
+        self.previous_reward = self.trading_sim.get_NAV()
+
+        self.render_data = {'buy': [], 'sell': [], 'hold': [], 'portfolio_value': [], 'spread': []}
+        self.plot_data = {}
+
+        date, data = next(self.data_source)
+        s1_price, s2_price, s1_pct, s2_pct = data
+
+        spread, data_ready = self.market_metrics.update(s1_price, s2_price)
+        while not data_ready:
+            date, data = next(self.data_source)
+            s1_price, s2_price, s1_pct, s2_pct = data
+
+            spread, data_ready = self.market_metrics.update(s1_price, s2_price)
+            self.trading_day += 1
+
+
+        return np.array([s1_pct, s2_pct, spread])
+
+    def skip_forward(self, days):
+        try:
+            for _ in range(days):
+                next(self.data_source)
+                self.trading_day += 1
+            return True
+        except StopIteration:
+            False
+    
+    def step(self, action):
+        done = 0 
+        try:
+            date, data = next(self.data_source)
+        except StopIteration:
+            done = 1
+            obs = [0, 0, 0]
+            reward = 0
+            return obs, reward, done, {}
+        s1_price, s2_price, s1_pct, s2_pct = data
+
+        spread, _ = self.market_metrics.update(s1_price, s2_price)
+
+        # self.trading_sim.execute(action, spread, s1_price, s2_price)
+        self.trading_sim.redistribute(action, s1_price, s2_price)
+
+        self.trading_day += 1
+
+        obs = np.array([s1_pct, s2_pct, spread])
+        self.trading_sim.update_values(s1_price, s2_price)
+        balance = self.trading_sim.get_NAV()
+        reward = balance / self.previous_balance - 1 # Subtract 1 to centre at 0
+
+        # if action == Actions.BUY.value:
+        #     self.render_data['buy'].append((self.trading_day, balance))
+        # elif action == Actions.SELL.value:
+        #     self.render_data['sell'].append((self.trading_day, balance))
+        # else:
+        #     self.render_data['hold'].append((self.trading_day, balance))
+        self.render_data['portfolio_value'].append((self.trading_day, balance))
+        self.render_data['spread'].append((self.trading_day, spread))
+
+        self.previous_balance = balance
+        return obs, reward, done, {"date": date, "trading_day": self.trading_day}
+
+    def render(self, mode='human'):
+        if mode=='human':
+            # Setting up matplotlib data structures
+            fig = self.plot_data.get('portfolio_value_fig') or plt.figure(figsize=(18, 4.5))
+            ax  = self.plot_data.get('portfolio_value_ax') or fig.add_subplot(121)
+            ax2  = self.plot_data.get('portfolio_value_ax2') or fig.add_subplot(122)
+
+            self.plot_data['portfolio_value_fig'] = fig
+            self.plot_data['portfolio_value_ax'] = ax
+            self.plot_data['portfolio_value_ax2'] = ax2
+
+            fig.suptitle("Pairs Trading with Machine Learning")
+
+            # Separate x and y data from lists of tuples
+            x_data = lambda data: [x[0] for x in data]
+            y_data = lambda data: [x[1] for x in data]
+
+            # Plot Portfolio values
+            ax.clear()
+            ax.set_title('Portfolio Value')
+            ax.plot(
+                x_data(self.render_data['portfolio_value']),
+                y_data(self.render_data['portfolio_value']),
+                label='Portfolio value'
+            )
+
+            # Plot Actions data
+            ax.plot(x_data(self.render_data['buy']), y_data(self.render_data['buy']), '.r', label='Buy')
+            ax.plot(x_data(self.render_data['sell']), y_data(self.render_data['sell']), '^g', label='Sell')
+            ax.plot(x_data(self.render_data['hold']), y_data(self.render_data['hold']), 'xy', label='Hold')
+            ax.legend(loc='best')
+
+            # Plot spread
+            ax2.clear()
+            ax2.set_title('Stock Price Spread')
+            ax2.plot(
+                x_data(self.render_data['spread']),
+                y_data(self.render_data['spread']),
+                color='#9467bd'
+            )
+
+            fig.canvas.draw()
+            fig.canvas.flush_events()
+            plt.pause(0.05)
+        elif mode=='console':
+            latest_data = self.render_data # [-1]
+
+            trading_day, nav, _, spread = latest_data
+
+            print(f"Trading day: {trading_day}. Portfolio Value: {nav}. Spread: {spread}")
+        else:
+            print("Invalid render mode")
+
 if __name__=='__main__':
-    env = PairsTradingEnv("AAPL","MSFT")
+    env = PairsTradingEnvV2("AAPL","AMZN", transaction_fee=10)
 
     import random
     import time
 
     env.reset()
     plt.ion()
-    for _ in range(50):
-        obs, reward, done, _ = env.step(random.randint(0,2))
+    for _ in range(500):
+        max_stock_dist = 0.8
+        dist_a = random.uniform(0,max_stock_dist)
+
+        action = [dist_a,max_stock_dist-dist_a,1-max_stock_dist]
+        action = [0.4, 0.4, 0.2]
+
+        obs, reward, done, _ = env.step(action)
         env.render(mode='human')
 
         if done: break
--- src/gym_pairs_trading/envs/trading_sim.py
+++ src/gym_pairs_trading/envs/trading_sim.py
@@ -1,4 +1,6 @@
 from enum import Enum
+import numpy as np
+import math
 
 class Status(Enum):
     INVESTED_IN_SPREAD = 1
@@ -105,13 +107,77 @@ class TradingSim(object):
 
         return (new_cash_balance, 0)
 
+class TradingSimV2(object):
+    def __init__(self, **kwargs):
+        start_balance = kwargs.get('start_balance', 10000)
+        transaction_fee = kwargs.get('transaction_fee', 50)
+        
+        self._values = np.array([0, 0, 1.0], dtype=np.float32)
+        self._balances = np.array([0, 0, start_balance], dtype=np.float32)
+        
+        self.start_balance = start_balance
+    
+        self.transaction_fee = transaction_fee
+
+    def reset(self):
+        self._values = np.array([0, 0, 1.0], dtype=np.float32)
+        self._balances = np.array([0, 0, self.start_balance], dtype=np.float32)
+
+    def update_values(self, stock_1_price, stock_2_price):
+        self._values[0] = stock_1_price
+        self._values[1] = stock_2_price
+
+    def get_NAV(self):
+        return np.sum(self._values * self._balances)
+
+    def get_distribution(self):
+        return self._values * self._balances / self.get_NAV()
+
+    def get_balance_delta(self, cash_delta, stock_price):
+        if cash_delta < 0:
+            units = -math.floor(-cash_delta / stock_price)
+            return units, cash_delta-units*stock_price, 
+        else:
+            units = math.floor(cash_delta / stock_price)
+            return units, cash_delta-units*stock_price, 
+
+    def redistribute(self, distribution, stock_1_price, stock_2_price):
+        assert (1.0 - np.sum(distribution)) < 0.001, "Invalid input"
+        
+        self.update_values(stock_1_price, stock_2_price)
+        current_NAV = self.get_NAV()
+        current_distribution = self.get_distribution()
+
+        distribution_delta = distribution - current_distribution
+        if np.sum(np.abs(distribution_delta)) < 0.05:
+            # Do not redistribute if delta is less that 5%
+            print("No distribtion")
+            return
+        else:
+            print("distribute")
+
+        monatary_delta = distribution_delta * current_NAV
+
+        stock_1_delta, cash_1_delta = self.get_balance_delta(monatary_delta[0], stock_1_price)
+        stock_2_delta, cash_2_delta = self.get_balance_delta(monatary_delta[1], stock_2_price)
+
+        cash_delta = monatary_delta[2]+cash_1_delta+cash_2_delta
+        value_delta = np.array([stock_1_delta, stock_2_delta, cash_delta], dtype=np.float32)
+        self._balances += value_delta
+        self._balances[2] -= self.transaction_fee*2
+
+        if self._balances[2] < 0:
+            raise ValueError("Out of money")
+
 
 if __name__=='__main__':
-    ts = TradingSim()
-    ts.execute(1, -0.5, 50, 50)
-    print(ts.get_NAV(50, 50))
-    print(ts.status)
+    ts = TradingSimV2()
+    # print(ts.get_NAV())
+    # print(ts.get_distribution())
+    ts.redistribute(np.array([0.2, 0.4, 0.4]), 60, 23.5)
+    print(ts.get_NAV())
+    ts.redistribute(np.array([0.0, 0.9, 0.1]), 60, 23.5)
+    print(ts.get_NAV())
 
-    ts.execute(2, 0.001, 51, 50)
-    print(ts.get_NAV(51, 50))
-    print(ts.status)
+
+
